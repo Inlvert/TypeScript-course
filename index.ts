da63178f0ff76612fd5c7b0a23dc5794505b5679
Boris Ushakov <inlvert@gmail.com>
2025-06-12 15:27:24 +0300

    lesson6 - Basic types: string, number, boolean

---
+++ index.ts
@@ -0,0 +1,11 @@
+// --------------- lesson6 - Basic types: string, number, boolean
+
+// const isBirthday: boolean = true;
+// let age: number = 40;
+// const userName: string = "John";
+
+// age = "40";
+
+// if (isBirthday) {
+//   console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+// }
