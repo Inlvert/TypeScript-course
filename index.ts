00f2517909d46126620228e76b39e2566616fbdb
Boris Ushakov <inlvert@gmail.com>
2025-06-19 18:51:38 +0300

    add l-8 type any

--- index.ts
+++ index.ts
@@ -12,17 +12,42 @@
 
 // --------------- lesson7 - use types in functions
 
-const isBirthdayData: boolean = true;
-let ageData: number = 40;
-const userNameData: string = "John";
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "John";
 
-function logBirMsg(isBirthday: boolean, age: number, userName: string): void | string {
-  if (isBirthday) {
-    return console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
-  } else {
-    return "Error"
-  }
-}
+// function logBirMsg(isBirthday: boolean, age: number, userName: string): void | string {
+//   if (isBirthday) {
+//     return console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+//   } else {
+//     return "Error"
+//   }
+// }
+
+// const logBirMsg2 = (isBirthday: boolean, age: number, userName: string): void | string => {
+//   if (isBirthday) {
+//     return console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+//   } else {
+//     return "Error"
+//   }
+// }
+
+// logBirMsg(true, 35, "Alex");
+// logBirMsg(isBirthdayData, ageData, userNameData);
+
+// --------------- lesson8 - type any
+
+let salary; //any
+
+const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}'
+
+// const userObj = JSON.parse(userData) //any
+
+const userObj: {isBirthdayData: boolean, ageData: number, userNameData: string} = JSON.parse(userData)
+
+console.log(userObj)
+
+const {isBirthdayData, ageData, userNameData} = userObj
 
 const logBirMsg2 = (isBirthday: boolean, age: number, userName: string): void | string => {
   if (isBirthday) {
@@ -32,5 +57,3 @@ const logBirMsg2 = (isBirthday: boolean, age: number, userName: string): void | 
   }
 }
 
-logBirMsg(true, 35, "Alex");
-logBirMsg(isBirthdayData, ageData, userNameData);
