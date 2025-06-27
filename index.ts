e76b3345b64612103daf2611ca7de8834e65a065
Boris Ushakov <inlvert@gmail.com>
2025-06-27 23:55:11 +0300

    add l - 25 test

--- index.ts
+++ index.ts
@@ -493,25 +493,120 @@
 
 // --------------- lesson24 - Interfaces vs Type
 
-interface IConfig {
-  protocol: "http" | "https";
-  port: 3000 | 5000;
+// interface IConfig {
+//   protocol: "http" | "https";
+//   port: 3000 | 5000;
+// }
+
+// interface IConfig {
+//   data: Date;
+// }
+
+// interface IRole {
+//   role: string;
+// }
+
+// interface IConfogWithIRole extends IConfig, IRole {}
+
+// const serverConfig: IConfogWithIRole = {
+//   protocol: "http",
+//   port: 5000,
+//   data: new Date(),
+//   role: "user",
+// };
+
+// type Config = {
+//   protocol: "http" | "https";
+//   port: 3000 | 5000;
+// };
+
+// type Role = {
+//   role: string;
+// };
+
+// type ConfigWithRole = Config & Role;
+
+// const serverConfig2: IConfogWithIRole = {
+//   protocol: "http",
+//   port: 5000,
+//   data: new Date(),
+//   role: "user",
+// };
+
+// --------------- lesson25 - test
+
+// структура данных склада с одеждой
+
+interface ClothesWarehouse {
+  jackets: "empty" | number;
+  hats: "empty" | number;
+  socks: "empty" | number;
+  pants: "empty" | number;
 }
 
-interface IConfig {
-  data: Date;
+// структура данных склада с канцтоварами
+
+interface StationeryWarehouse {
+  scissors: "empty" | number;
+  paper: "empty" | boolean;
 }
 
-interface IRole {
-  role: string;
+// структура данных склада с бытовой техникой
+
+interface AppliancesWarehouse {
+  dishwashers: "empty" | number;
+  cookers: "empty" | number;
+  mixers: "empty" | number;
 }
 
-interface IConfogWithIRole extends IConfig, IRole {}
+// общая структура данных, наследует все данные из трех выше
+// + добавляет свои
 
-const serverConfig: IConfogWithIRole = {
-  protocol: "http",
-  port: 5000,
-  data: new Date(),
-  role: "user",
+interface TotalWarehouse
+  extends ClothesWarehouse,
+    StationeryWarehouse,
+    AppliancesWarehouse {
+  deficit: boolean;
+  date: Date;
+}
+
+// главный объект со всеми данными, должен подходить под формат TotalWarehouse
+
+const totalData: TotalWarehouse = {
+  jackets: 5,
+  hats: "empty",
+  socks: "empty",
+  pants: 15,
+  scissors: 15,
+  paper: true,
+  dishwashers: 3,
+  cookers: "empty",
+  mixers: 14,
+  date: new Date(),
+  deficit: true,
 };
 
+// Реализуйте функцию, которая принимает в себя главный объект totalData нужного формата
+// и возвращает всегда строку
+// Функция должна отфильтровать данные из объекта и оставить только те названия товаров, у которых значение "empty"
+// и поместить их в эту строку. Если таких товаров нет - возвращается другая строка (см ниже)
+
+// С данным объектом totalData строка будет выглядеть:
+// "We need this items: hats, socks, cookers"
+// Товары через запятую, в конце её не должно быть. Пробел после двоеточия, в конце строки его нет.
+
+function printReport(data: TotalWarehouse): string {
+  const result = Object.entries(data)
+    .filter((item) => item[1] === "empty")
+    .reduce((res, item) => `${res} ${item[0]},`, "");
+
+  console.log(result);
+
+  if (result) {
+    return `We need this items: ${result}`;
+  } else {
+    return "Everything fine";
+  }
+}
+
+console.log(printReport(totalData));
