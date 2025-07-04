6839637302744ee19961336b1f8f0637bb489f52
Boris Ushakov <inlvert@gmail.com>
2025-07-04 18:52:04 +0300

    add l - 42 - task Dom

--- index.js
+++ index.js
@@ -1077,12 +1077,64 @@
 // };
 // console.log(checkAnimalData(cheackAnimal))
 // --------------- lesson41 - work with DOM
-var hElem = document.createElement("h1");
-hElem.textContent = "test";
-document.body.append(hElem);
-var link = document.querySelector('h1');
-var counter = 0;
-link.addEventListener("click", function (e) {
-    counter++;
-    console.log("Clicked", counter);
+// const hElem = document.createElement("h1");
+// hElem.textContent = "test";
+// document.body.append(hElem);
+// const link = document.querySelector('h1') as HTMLElement;
+// let counter = 0;
+// link.addEventListener("click", (e) => {
+//   counter++;
+//   console.log("Clicked", counter);
+// });
+// --------------- lesson42 task Dom
+var forms = document.querySelectorAll("form");
+var email = document.getElementById("email");
+var title = document.getElementById("title");
+var text = document.getElementById("text");
+var checkbox = document.getElementById("checkbox");
+var formData = {
+    email: "",
+    title: "",
+    text: "",
+    checkbox: false,
+};
+forms.forEach(function (form) {
+    return form.addEventListener("submit", function (e) {
+        var _a, _b, _c, _d;
+        e.preventDefault();
+        formData.email = (_a = email === null || email === void 0 ? void 0 : email.value) !== null && _a !== void 0 ? _a : "";
+        formData.title = (_b = title === null || title === void 0 ? void 0 : title.value) !== null && _b !== void 0 ? _b : "";
+        formData.text = (_c = text === null || text === void 0 ? void 0 : text.value) !== null && _c !== void 0 ? _c : "";
+        formData.checkbox = (_d = checkbox === null || checkbox === void 0 ? void 0 : checkbox.checked) !== null && _d !== void 0 ? _d : false;
+        if (validateFormData(formData)) {
+            checkFormData(formData);
+            form.reset();
+        }
+    });
 });
+// Последовательность действий:
+// 1) Происходит submit любой из форм
+// 2) Все данные из 4х полей со страницы переходят в свойства объекта formData
+// 3) Запускается функция validateFormData с этим объектом, возвращает true/false
+// 4) Если на предыдущем этапе true, то запускается функция checkFormData с этим объектом
+function validateFormData(data) {
+    // Если каждое из свойств объекта data правдиво...
+    if (Object.values(data).every(function (value) { return value; })) {
+        return true;
+    }
+    else {
+        console.log("Please, complete all fields");
+        return false;
+    }
+}
+function checkFormData(data) {
+    var email = data.email;
+    var emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+    // Если email совпадает хотя бы с одним из массива
+    if (emails.some(function (e) { return e === email; })) {
+        console.log("This email is already exist");
+    }
+    else {
+        console.log("Posting data...");
+    }
+}
