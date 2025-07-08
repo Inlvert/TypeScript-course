2157b703114b338df834b223bafb09b12a300bd2
Boris Ushakov <inlvert@gmail.com>
2025-07-08 14:23:40 +0300

    add l - 54 - task

--- index.ts
+++ index.ts
@@ -1904,61 +1904,138 @@
 
 // --------------- lesson53 - Indexed Access Types
 
-interface ICompany {
-  name: string;
-  debts: number;
-  departments: Department[];
-  managment: {
-    owner: string;
-  };
-}
+// interface ICompany {
+//   name: string;
+//   debts: number;
+//   departments: Department[];
+//   managment: {
+//     owner: string;
+//   };
+// }
 
-interface Department {
-  [key: string]: string;
-}
+// interface Department {
+//   [key: string]: string;
+// }
 
-// const debts = 'debts'; // V1
-// let debts = "debts" as "debts"; // V2
-let debts: "debts" = "debts"; // V3
+// // const debts = 'debts'; // V1
+// // let debts = "debts" as "debts"; // V2
+// let debts: "debts" = "debts"; // V3
 
-// type CompanyDebtsType = ICompany["debts"];
-type CompanyDebtsType = ICompany[typeof debts];
-type CompanyOwnerType = ICompany["managment"]["owner"];
-type CompanyDepType = ICompany["departments"];
-type CompanyDepTypes = ICompany["departments"][number];
+// // type CompanyDebtsType = ICompany["debts"];
+// type CompanyDebtsType = ICompany[typeof debts];
+// type CompanyOwnerType = ICompany["managment"]["owner"];
+// type CompanyDepType = ICompany["departments"];
+// type CompanyDepTypes = ICompany["departments"][number];
 
-type Test = ICompany[keyof ICompany];
+// type Test = ICompany[keyof ICompany];
 
-type CompanyKeys = keyof ICompany;
-const keys: CompanyKeys = "name";
+// type CompanyKeys = keyof ICompany;
+// const keys: CompanyKeys = "name";
 
-// const hh: ICompany = {
-//   name: "HH",
-//   debts: 5000,
+// // const hh: ICompany = {
+// //   name: "HH",
+// //   debts: 5000,
+// // };
+
+// function printDebts<T, K extends keyof T, S extends keyof T>(
+//   company: T,
+//   name: K,
+//   debts: S
+// ) {
+//   console.log(`Company ${company[name]}, debts ${company[debts]}`);
+// }
+
+// const google: ICompany = {
+//   name: "Google",
+//   debts: 10000,
+//   departments: [
+//     {
+//       sales: "sales",
+//       developer: "dev",
+//     },
+//   ],
+//   managment: {
+//     owner: "Jhon",
+//   },
 // };
 
-function printDebts<T, K extends keyof T, S extends keyof T>(
-  company: T,
-  name: K,
-  debts: S
-) {
-  console.log(`Company ${company[name]}, debts ${company[debts]}`);
+// type GoogleKeys = keyof typeof google;
+
+// const keys2: GoogleKeys = "name";
+
+// --------------- lesson54 - task
+
+interface IPhone {
+  company: string;
+  number: number;
 }
 
-const google: ICompany = {
-  name: "Google",
-  debts: 10000,
-  departments: [
-    {
-      sales: "sales",
-      developer: "dev",
-    },
-  ],
-  managment: {
-    owner: "Jhon",
+// IMobilePhone должен наследоваться от IPhone,
+// тип свойства companyPartner зависит от свойства company
+
+interface IMobilePhone extends IPhone {
+  size: string;
+  companyPartner: IPhone["company"];
+  manufactured: Date;
+}
+
+// Типизировать объект phones
+
+const phones: IMobilePhone[] = [
+  {
+    company: "Nokia",
+    number: 1285637,
+    size: "5.5",
+    companyPartner: "MobileNokia",
+    manufactured: new Date("2022-09-01"),
   },
-};
+  {
+    company: "Samsung",
+    number: 4356637,
+    size: "5.0",
+    companyPartner: "SamMobile",
+    manufactured: new Date("2021-11-05"),
+  },
+  {
+    company: "Apple",
+    number: 4552833,
+    size: "5.7",
+    companyPartner: "no data",
+    manufactured: new Date("2022-05-24T12:00:00"),
+  },
+];
 
-type GoogleKeys = keyof typeof google;
+interface IPhonesManufacturedAfterDate extends IMobilePhone {
+  initialDate: string;
+}
 
-const keys2: GoogleKeys = "name";
+// Функция должна отфильтровать массив данных и вернуть новый массив
+// с телефонами, выпущенными после даты в третьем аргументе
+
+function filterPhonesByDate<T, K extends keyof T>(
+  phones: T[],
+  key: K,
+  initial: string
+): Partial<IPhonesManufacturedAfterDate>[] {
+  return phones
+    .filter((phone) => {
+      const manufactured = phone[key];
+
+      if (
+        manufactured instanceof Date &&
+        manufactured.getTime() > new Date(initial).getTime()
+      ) {
+        return phone;
+      }
+    })
+    .map((phone) => {
+      const newObj = { ...phone, initialDate: initial };
+      return newObj;
+    });
+}
+
+// Второй аргумент при вызове функции должен быть связан с первым,
+// а значит мы получим подсказки - свойства этого объекта
+
+console.log(filterPhonesByDate(phones, "manufactured", "2022-01-01"));
+
