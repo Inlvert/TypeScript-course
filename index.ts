6f5ab95f961932cdbe0db1536d0b7cc30217a554
Boris Ushakov <inlvert@gmail.com>
2025-07-08 18:11:13 +0300

    add l - 57 - Tamplate literal types

--- index.ts
+++ index.ts
@@ -2121,40 +2121,60 @@
 
 // --------------- lesson56 - Mapped types, +/-operators
 
+// type Currencies = {
+//   readonly usa: "usd";
+//   china?: "cny";
+//   readonly ukraine: "uah";
+//   kz?: "tenge";
+// };
+
+// type ROnlyCurr = Readonly<Currencies>;
+
+// type CustomCurrencies = {
+//   usa: string;
+//   china: string;
+//   ukraine: string;
+//   kz: string;
+// };
+
+// type CreateCustomCurr<T> = {
+//   +readonly[P in keyof T]-?: string;
+// };
+
+// type CustomCurrencies2 = CreateCustomCurr<Currencies>
+
+// // type СопоставимийТип = {
+// //   [произвольнийИдентификатор in Множество]: ПроизвольнийТипДанних;
+// // };
+
+// type Keys = "name" | "age" | "role";
+
+// type User = {
+//   [K in Keys]: string;
+// };
+
+// const alex: User = {
+//   name: "Alex",
+//   age: "25",
+//   role: "admin",
+// };
+
+// --------------- lesson57 - Tamplate literal types
+
 type Currencies = {
-  readonly usa: "usd";
+  usa: "usd";
   china?: "cny";
-  readonly ukraine: "uah";
+  ukraine: "uah";
   kz?: "tenge";
 };
 
-type ROnlyCurr = Readonly<Currencies>;
-
-type CustomCurrencies = {
-  usa: string;
-  china: string;
-  ukraine: string;
-  kz: string;
-};
-
 type CreateCustomCurr<T> = {
-  +readonly[P in keyof T]-?: string;
+  [P in keyof T as `custom${Capitalize<string & P>}`]: string;
 };
 
-type CustomCurrencies2 = CreateCustomCurr<Currencies>
+type CustomCurrencies = CreateCustomCurr<Currencies>;
 
-// type СопоставимийТип = {
-//   [произвольнийИдентификатор in Множество]: ПроизвольнийТипДанних;
-// };
+type MyAnimation = "fade" | "swipe";
+type Direction = "in" | "out";
 
-type Keys = "name" | "age" | "role";
-
-type User = {
-  [K in Keys]: string;
-};
-
-const alex: User = {
-  name: "Alex",
-  age: "25",
-  role: "admin",
-};
+type MyNewAnimation = `${MyAnimation}${Capitalize<Direction>}`;
