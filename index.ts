d1746a655bb6664e793bce9572d01546eb94b462
Boris Ushakov <inlvert@gmail.com>
2025-07-09 15:18:42 +0300

    add l - 59 task 2

--- index.ts
+++ index.ts
@@ -2233,105 +2233,126 @@ type MyNewAnimation = `${MyAnimation}${Capitalize<Direction>}`;
 // Простыми словами: при добавлении свойства в целевой объект они должны быть
 // автоматически добавлены в зависимые (сразу подсказка от TS)
 
-interface IClasses {
-  name: string;
-  startsAt?: string;
-  duration: number;
+// interface IClasses {
+//   name: string;
+//   startsAt?: string;
+//   duration: number;
+// }
+
+// interface IFutureClasses extends Omit<IClasses, "startsAt"> {
+//   willStartsAt: string;
+// }
+
+// interface ICurrClients {
+//   name: string;
+//   age: string | number;
+//   gender: "male" | "female";
+//   timeLeft: string;
+//   makeCallFor: Date;
+// }
+
+// type CurrClientsType = Omit<ICurrClients, 'makeCallFor'>
+
+// type IExClients = Omit<ICurrClients, "timeLeft">
+
+// type IFutureClients = Pick<IExClients, 'name' | 'makeCallFor'>
+
+// interface IFitnessClubCenter {
+//   clubName: string;
+//   location: string;
+//   classes: IClasses[];
+//   currClients: CurrClientsType[];
+//   futureClasses: IFutureClasses[];
+//   exClients: IExClients[];
+//   futureClients: IFutureClients[];
+// }
+
+// const fitnessClubCenter: IFitnessClubCenter = {
+//   clubName: "Fitness club Center",
+//   location: "central ave. 45, 5th floor",
+//   classes: [
+//     {
+//       name: "yoga",
+//       startsAt: "8:00 AM",
+//       duration: 60,
+//     },
+//     {
+//       name: "trx",
+//       startsAt: "11:00 AM",
+//       duration: 45,
+//     },
+//     {
+//       name: "swimming",
+//       startsAt: "3:00 PM",
+//       duration: 70,
+//     },
+//   ],
+//   futureClasses: [
+//     {
+//       name: "boxing",
+//       willStartsAt: "6:00 PM",
+//       duration: 40,
+//     },
+//     {
+//       name: "breath training",
+//       willStartsAt: "8:00 PM",
+//       duration: 30,
+//     },
+//   ],
+//   currClients: [
+//     {
+//       name: "John Smith",
+//       age: "-",
+//       gender: "male",
+//       timeLeft: "1 month",
+//     },
+//     {
+//       name: "Alise Smith",
+//       age: 35,
+//       gender: "female",
+//       timeLeft: "3 month",
+//     },
+//     {
+//       name: "Ann Sonne",
+//       age: 24,
+//       gender: "female",
+//       timeLeft: "5 month",
+//     },
+//   ],
+//   exClients: [
+//     {
+//       name: "Tom Smooth",
+//       age: 50,
+//       gender: "male",
+//       makeCallFor: new Date("2023-08-12"),
+//     },
+//   ],
+//   futureClients: [
+//     {
+//       name: "Maria",
+//       makeCallFor: new Date("2023-07-10"),
+//     },
+//   ],
+// };
+
+// --------------- lesson59 task 2
+
+interface IForm {
+  login: string;
+  password: string;
 }
 
-interface IFutureClasses extends Omit<IClasses, "startsAt"> {
-  willStartsAt: string;
-}
+// Необходимо типизировать объект валидации
+// Учтите, что данные в форме могут расширяться и эти поля
+// должны появиться и в объекте валидации
 
-interface ICurrClients {
-  name: string;
-  age: string | number;
-  gender: "male" | "female";
-  timeLeft: string;
-  makeCallFor: Date;
-}
+const validationData: Validation<IForm> = {
+  login: { isValid: false, errorMsg: "At least 3 characters" },
+  password: { isValid: true },
+};
 
-type CurrClientsType = Omit<ICurrClients, 'makeCallFor'>
-
-type IExClients = Omit<ICurrClients, "timeLeft">
-
-
-type IFutureClients = Pick<IExClients, 'name' | 'makeCallFor'>
-
-interface IFitnessClubCenter {
-  clubName: string;
-  location: string;
-  classes: IClasses[];
-  currClients: CurrClientsType[];
-  futureClasses: IFutureClasses[];
-  exClients: IExClients[];
-  futureClients: IFutureClients[];
-}
-
-const fitnessClubCenter: IFitnessClubCenter = {
-  clubName: "Fitness club Center",
-  location: "central ave. 45, 5th floor",
-  classes: [
-    {
-      name: "yoga",
-      startsAt: "8:00 AM",
-      duration: 60,
-    },
-    {
-      name: "trx",
-      startsAt: "11:00 AM",
-      duration: 45,
-    },
-    {
-      name: "swimming",
-      startsAt: "3:00 PM",
-      duration: 70,
-    },
-  ],
-  futureClasses: [
-    {
-      name: "boxing",
-      willStartsAt: "6:00 PM",
-      duration: 40,
-    },
-    {
-      name: "breath training",
-      willStartsAt: "8:00 PM",
-      duration: 30,
-    },
-  ],
-  currClients: [
-    {
-      name: "John Smith",
-      age: "-",
-      gender: "male",
-      timeLeft: "1 month",
-    },
-    {
-      name: "Alise Smith",
-      age: 35,
-      gender: "female",
-      timeLeft: "3 month",
-    },
-    {
-      name: "Ann Sonne",
-      age: 24,
-      gender: "female",
-      timeLeft: "5 month",
-    },
-  ],
-  exClients: [
-    {
-      name: "Tom Smooth",
-      age: 50,
-      gender: "male",
-      makeCallFor: new Date("2023-08-12"),
-    },
-  ],
-  futureClients: [
-    {
-      name: "Maria",
-      makeCallFor: new Date("2023-07-10"),
-    },
-  ],
+type Validation<T> = {
+  [P in keyof T]:
+    | { isValid: true }
+    | { isValid: false; errorMsg: "At least 3 characters" };
 };
