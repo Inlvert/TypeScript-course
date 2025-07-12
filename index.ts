bed1035710b95d1bd3d3ed2ce26268e20f4dbac6
Boris Ushakov <inlvert@gmail.com>
2025-07-12 18:02:53 +0300

    add l - 70 - task

--- index.ts
+++ index.ts
@@ -2691,22 +2691,93 @@
 
 // --------------- lesson69 - implements in class
 
-interface IUser {
-  login: string;
-  password: string;
+// interface IUser {
+//   login: string;
+//   password: string;
+// }
+
+// interface IValidation {
+//   valid: boolean;
+//   isValid: (data: string) => boolean;
+// }
+
+// class UserForm implements IUser, IValidation {
+//   login!: string;
+//   password!: string;
+//   valid!: boolean;
+//   isValid(login: string) {
+//     return login.length > 3;
+//   }
+// }
+
+// --------------- lesson70 - task
+
+enum TransferStatus {
+  Pending = "pending",
+  Rejected = "rejected",
+  Completed = "completed",
 }
 
-interface IValidation {
-  valid: boolean;
-  isValid: (data: string) => boolean;
+enum ErrorMessages {
+  NotFound = "Not found: 404",
+  NotEnoughSpace = "Not enough space: 507",
+  Forbidden = "Forbidden: 403",
 }
 
-class UserForm implements IUser, IValidation {
-  login!: string;
-  password!: string;
-  valid!: boolean;
-  isValid(login: string) {
-    return login.length > 3;
+interface ITransfer {
+  path: string;
+  data: string[];
+  date?: Date;
+  start: (p: string, d: string[]) => string;
+  stop: (reason: string) => string;
+}
+
+interface TransferError {
+  message: ErrorMessages;
+}
+
+// Класс должен имплементировать ITransfer и TransferError
+class SingleFileTransfer implements ITransfer, TransferError {
+  path!: string;
+  data!: string[];
+
+  message!: ErrorMessages;
+  transferStatus!: TransferStatus;
+
+  // Место для реализаций
+  // Необходимо создать метод checkTransferStatus, проверяющий состояние передачи данных
+  // Можно вывести в консоль данные, можно вернуть строку
+
+  constructor(status: TransferStatus) {
+    this.transferStatus = status;
+  }
+
+  checkTransferStatus(): string {
+    return this.transferStatus;
+  }
+
+  start(p: string, d: string[]): string {
+    return "";
+  }
+
+  // Необходимо создать метод, который будет останавливать передачу данных
+  // И возвращать строку с причиной и датой остановки (Дата в любом формате)
+
+  stop = (reason: string): string => {
+    return `The transfer was stoped, reason is: ${reason}, Date: ${new Date().toLocaleString()}`;
+  };
+
+  // Необходимо создать метод, который будет возвращать строку, содержащую
+  // Статус передачи и любое сообщение об ошибке. На ваш выбор или отталкиваться от приходящего аргумента
+  // Метод может показаться странным, но может использоваться для тестов, например
+
+  error(): string {
+    return `${TransferStatus.Rejected} error message is ${ErrorMessages.NotFound}`;
   }
 }
 
+const test = new SingleFileTransfer(TransferStatus.Pending);
+
+console.log(test.checkTransferStatus());
+console.log(test.stop("WTF?"));
+console.log(test.error());
