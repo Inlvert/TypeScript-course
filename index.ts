90097df58db9e0a48d7a6c86f2e310ba6872f2c5
Boris Ushakov <inlvert@gmail.com>
2025-07-16 14:31:34 +0300

    add l - 79 - Терминология, фабрика декораторов

--- index.ts
+++ index.ts
@@ -3266,13 +3266,53 @@
 
 // --------------- lesson78 - Decorators in TS
 
+// interface ICar {
+//   fuel: string;
+//   open: boolean;
+//   freeSeats: number;
+// }
+// @closeCar
+// @addFuel
+// class MyCar implements ICar {
+//   fuel: string = "50%";
+//   open: boolean = true;
+//   freeSeats: number;
+//   isOpen() {
+//     console.log(this.fuel);
+//     return this.open ? "open" : "close";
+//   }
+// }
+
+// function closeCar<T extends { new (...args: any[]): {} }>(constructor: T) {
+//   return class extends constructor {
+//     open = false;
+//   };
+// }
+
+// function addFuel<T extends { new (...args: any[]): {} }>(constructor: T) {
+//   return class extends constructor {
+//     fuel = "100%";
+//   };
+// }
+
+// const car = new MyCar();
+// console.log(car);
+
+// car.isOpen();
+// console.log("car.open", car.open);
+
+// console.log("car.fuel", car.fuel);
+
+// --------------- lesson79 - Терминология, фабрика декораторов
+
 interface ICar {
   fuel: string;
   open: boolean;
   freeSeats: number;
 }
-@closeCar
-@addFuel
+
+@changeDoorStatus(true)
+@changeAmountOfFuel('90%')
 class MyCar implements ICar {
   fuel: string = "50%";
   open: boolean = true;
@@ -3283,17 +3323,37 @@ class MyCar implements ICar {
   }
 }
 
-function closeCar<T extends { new (...args: any[]): {} }>(constructor: T) {
-  return class extends constructor {
-    open = false;
+function changeDoorStatus(status: boolean) {
+  console.log('1')
+  return <T extends { new (...args: any[]): {} }>(constructor: T) => {
+    console.log('2')
+    return class extends constructor {
+      open = status;
+    };
   };
 }
 
-function addFuel<T extends { new (...args: any[]): {} }>(constructor: T) {
-  return class extends constructor {
-    fuel = "100%";
+function changeAmountOfFuel(amount: string) {
+  console.log('3')
+  return <T extends { new (...args: any[]): {} }>(constructor: T) => {
+    console.log('4')
+    return class extends constructor {
+      fuel = amount;
+    };
   };
 }
+
+// function closeCar<T extends { new (...args: any[]): {} }>(constructor: T) {
+//   return class extends constructor {
+//     open = false;
+//   };
+// }
+
+// function addFuel<T extends { new (...args: any[]): {} }>(constructor: T) {
+//   return class extends constructor {
+//     fuel = "100%";
+//   };
+// }
 
 const car = new MyCar();
 console.log(car);
@@ -3302,3 +3362,5 @@ car.isOpen();
 console.log("car.open", car.open);
 
 console.log("car.fuel", car.fuel);
+
+// 1 3 4 2
